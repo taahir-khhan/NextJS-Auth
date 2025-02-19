10aea4187eb1426ef277e73831357e8a588783a1
Tahir Khan <tahirtrk42@gmail.com>
2025-02-19 16:47:18 +0530

    CORS setup for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
